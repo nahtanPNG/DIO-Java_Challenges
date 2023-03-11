430114ec4a9ad63977c07a0f69f2a0fa6b48f70a
nahtanPNG <nathan.ferreiira13@gmail.com>
2023-03-11 12:41:13 -0300

    Changing folders

---
+++ desafios-básicos/src/edu/nathan/dio/desafios/basico/PoderLuta.java
@@ -0,0 +1,23 @@
+package edu.nathan.dio.desafios.basico;
+import java.util.*;
+
+public class PoderLuta{
+
+    public static void main(String[] args){
+        int casos, poderDeLuta;
+
+        Scanner ler = new Scanner(System.in);
+
+        casos = ler.nextInt();
+
+        for(int i = 0; i < casos; i++){
+            poderDeLuta = ler.nextInt();
+
+            if(poderDeLuta > 8000){
+                System.out.println("Mais de 8000!");
+            }else{
+                System.out.println("Inseto!");
+            }
+        }
+    }
+}
